00837a63937c2661e4120b87446bbfe89c6437f0
yuviieee <yuvrajguleria45@gmail.com>
2025-11-02 12:31:29 +0530

    Time: 490 ms (38.46%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
 select w1.id
 from Weather w1 inner join Weather w2
+
+-- datediff(w1.recordDate, w2.recordDate) = 1 
+-- means: "Only keep the pairs where the date in w1 is exactly one day after the date in w2."
+-- So, w1 effectively becomes "Today" and w2 becomes "Yesterday".
+
 where datediff(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature

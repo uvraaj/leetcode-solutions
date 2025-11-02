38bead8565bd7c590f19e5c430875d5e591e4c5e
Yuvraj Guleria <yuvrajguleria45@gmail.com>
2025-11-03 00:52:28 +0530

    Time: 623 ms (56.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,14 @@
+SELECT 
+    s.user_id,
+    ROUND(
+        IFNULL(
+            AVG(CASE WHEN c.action = 'confirmed' THEN 1.0 ELSE 0.0 END), 
+            0
+        ), 
+    2) AS confirmation_rate
+FROM 
+    Signups s
+LEFT JOIN 
+    Confirmations c ON s.user_id = c.user_id
+GROUP BY 
+    s.user_id;

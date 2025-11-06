2f0b8bf847f337ec9a7d036087f5a4fa44ba3d37
Yuvraj Guleria <yuvrajguleria45@gmail.com>
2025-11-06 23:53:11 +0530

    Time: 2215 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,10 @@
-# Write your MySQL query statement below
-SELECT 
-    q1.person_name
-FROM Queue q1 JOIN Queue q2 ON q1.turn >= q2.turn
-GROUP BY q1.turn
-HAVING SUM(q2.weight) <= 1000
-ORDER BY SUM(q2.weight) DESC
-LIMIT 1
+WITH cte AS
+(
+    SELECT person_name, SUM(weight) OVER(ORDER BY turn ASC) AS sum
+    FROM Queue
+)
+SELECT person_name
+FROM cte
+WHERE sum <= 1000
+ORDER BY sum DESC
+LIMIT 1;

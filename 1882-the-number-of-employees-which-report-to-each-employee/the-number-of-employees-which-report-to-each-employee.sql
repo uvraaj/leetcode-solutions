7c577c950bda20b98ec4db4ae8409e7cf540bd00
Yuvraj Guleria <yuvrajguleria45@gmail.com>
2025-11-06 11:19:43 +0530

    Time: 576 ms (73.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,15 @@
+select
+    m.employee_id,
+    m.name,
+    count(e.employee_id) as reports_count,
+    round(avg(e.age)) as average_age
+from 
+    Employees as e
+join
+    Employees as m
+on
+    e.reports_to = m.employee_id
+group by
+    m.employee_id, m.name
+order by
+    m.employee_id;

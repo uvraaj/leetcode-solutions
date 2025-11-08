810031fe7952102194beac9750392edb6ba37ffa
Yuvraj Guleria <yuvrajguleria45@gmail.com>
2025-11-08 16:25:12 +0530

    Time: 1809 ms (13.34%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,5 +1,28 @@
-SELECT "Low Salary" as category, sum(if(income<20000,1,0)) AS accounts_count FROM Accounts
-union
-SELECT "Average Salary" as category, sum(if(income>=20000 and income<=50000,1,0)) AS accounts_count FROM Accounts
-union
-SELECT "High Salary" as category, sum(if(income>50000,1,0)) AS accounts_count FROM Accounts
+with salary_count as(
+    select
+        sum(case when income < 20000 then 1 else 0 end) as low_count,
+        sum(case when income >= 20000 and income <= 50000 then 1 else 0 end) as avg_count,
+        sum(case when income > 50000 then 1 else 0 end) as high_count
+    from Accounts
+)
+
+select 
+    "Low Salary" as category,
+    low_count as accounts_count
+from salary_count
+union all
+select
+    "Average Salary" as category,
+    avg_count as accounts_count
+from salary_count
+union all
+select
+    "High Salary" as category,
+    high_count as accounts_count
+from salary_count
+order by
+    accounts_count desc;
+
+
+
+

4a03a7247c9ba8d06a27b857d2ca6ab39e9889e7
Yuvraj Guleria <yuvrajguleria45@gmail.com>
2025-11-08 15:33:58 +0530

    Time: 348 ms (28.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,11 @@
+select 
+    employee_id 
+from 
+    Employees as e 
+where 
+    salary < 30000 and 
+    manager_id not in (
+        select employee_id
+        from Employees
+    )
+order by employee_id;

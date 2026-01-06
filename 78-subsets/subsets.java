6e2040981ca2e65cd963e90d0a65b1fa46705ba1
Yuvraj Singh Guleria <yuvrajguleria45@gmail.com>
2026-01-07 00:44:47 +0530

    Time: 1 ms (87.94%) | Memory: 44.3 MB (37.51%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,21 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-
-        List<List<Integer>> list = new ArrayList<>();
-        backtrack(list, new ArrayList<>(), nums, 0);
-        return list;
-    }
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start){
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
         
-        list.add(new ArrayList<>(tempList));
-
-        for (int i = start; i < nums.length; i++) {
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, i + 1);
-            tempList.remove(tempList.size() - 1);
+        // Loop from 0 to 2^n (e.g., 0 to 7)
+        for (int i = 0; i < (1 << n); i++) {
+            List<Integer> subset = new ArrayList<>();
+            
+            // Check every bit position
+            for (int j = 0; j < n; j++) {
+                // If the j-th bit is set, include nums[j]
+                if ((i >> j & 1) == 1) {
+                    subset.add(nums[j]);
+                }
+            }
+            result.add(subset);
         }
+        return result;
     }
 }

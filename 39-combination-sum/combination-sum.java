683d13dfb3762339fc800b779182f2e6140c8531
Yuvraj Singh Guleria <yuvrajguleria45@gmail.com>
2026-01-07 12:10:33 +0530

    Time: 2 ms (95.34%) | Memory: 45.9 MB (36.21%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -11,13 +11,18 @@ class Solution {
         //base case: if checked for all elements of the candidates array
         if (curr_index == candidates.length) {
             if (target == 0) {
+                //if target is zero return empty list
                 result.add(new ArrayList<>(tempList));
             }
             return;
         }
+        //there is a chance of finding a valid combination
         if (candidates[curr_index] <= target) {
+            //adding the possible number to the temporay list
             tempList.add(candidates[curr_index]);
+            //finding another posiible element via recursion
             generate_combinations(curr_index, candidates, target - candidates[curr_index], tempList, result);
+            //backtracking to the previous stage in order to find another combinations
             tempList.remove(tempList.size() - 1);
         }
         //moving to next element

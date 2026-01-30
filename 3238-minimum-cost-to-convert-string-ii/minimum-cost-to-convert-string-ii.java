6566d68157e04a487e6a9cd87d782ab31fc0f5c7
Yuvraj Singh Guleria <yuvrajguleria45@gmail.com>
2026-01-30 05:35:14 +0530

    Time: 100 ms (100.00%) | Memory: 47.7 MB (97.67%) - LeetSync

--- 3238-minimum-cost-to-convert-string-ii/minimum-cost-to-convert-string-ii.java
+++ 3238-minimum-cost-to-convert-string-ii/minimum-cost-to-convert-string-ii.java
@@ -1,59 +1,62 @@
 class Solution {
+    private int index = 0;
     public long minimumCost(String source, String target, String[] original, String[] changed, int[] cost) {
-        HashMap<String, Integer> index = new HashMap<>();
-        for (String o : original) {
-            if (!index.containsKey(o)) {
-                index.put(o, index.size());
-            }
+        TrieNode root = new TrieNode();
+        for(String s : original) insert(s, root);
+        for(String s : changed) insert(s, root);
+        int[][] dist = new int[index][index];
+        for(int i = 0; i < index; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+            dist[i][i] = 0;
         }
-        for (String c : changed) {
-            if (!index.containsKey(c)) {
-                index.put(c, index.size());
-            }
+        for(int i = 0; i < cost.length; i++) {
+            int x = getIndex(original[i], root), y = getIndex(changed[i], root);
+            if(cost[i] < dist[x][y]) dist[x][y] = cost[i];
         }
-        long[][] dis = new long[index.size()][index.size()];
-        for (int i = 0; i < dis.length; i++) {
-            Arrays.fill(dis[i], Long.MAX_VALUE);
-            dis[i][i] = 0;
-        }
-        for (int i = 0; i < cost.length; i++) {
-            dis[index.get(original[i])][index.get(changed[i])] = Math.min(dis[index.get(original[i])][index.get(changed[i])], (long)cost[i]);
-        }
-        for (int k = 0; k < dis.length; k++) {
-            for (int i = 0; i < dis.length; i++)
-                if (dis[i][k] < Long.MAX_VALUE) {
-                    for (int j = 0; j < dis.length; j++) {
-                        if (dis[k][j] < Long.MAX_VALUE) {
-                            dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
-                        }
+        for(int i = 0; i < index; i++) {
+            for(int j = 0; j < index; j++) {
+                if(dist[j][i] != Integer.MAX_VALUE) {
+                    for(int k = 0; k < index; k++) {
+                        if(dist[i][k] != Integer.MAX_VALUE && dist[j][i] + dist[i][k] < dist[j][k]) dist[j][k] = dist[j][i] + dist[i][k];
                     }
                 }
+            }
         }
-        HashSet<Integer> set = new HashSet<>();
-        for (String o : original) {
-            set.add(o.length());
-        }
-        long[] dp = new long[target.length() + 1];
+        char[] arr1 = source.toCharArray(), arr2 = target.toCharArray();
+        int n = arr1.length;
+        long[] dp = new long[n + 1];
         Arrays.fill(dp, Long.MAX_VALUE);
-        dp[0] = 0L;
-        for (int i = 0; i < target.length(); i++) {
-            if (dp[i] == Long.MAX_VALUE) {
-                continue;
-            }
-            if (target.charAt(i) == source.charAt(i)) {
-                dp[i + 1] = Math.min(dp[i + 1], dp[i]);
-            }
-            for (int t : set) {
-                if (i + t >= dp.length) {
-                    continue;
-                }
-                int c1 = index.getOrDefault(source.substring(i, i + t), -1);
-                int c2 = index.getOrDefault(target.substring(i, i + t), -1);
-                if (c1 >= 0 && c2 >= 0 && dis[c1][c2] < Long.MAX_VALUE) {
-                    dp[i + t] = Math.min(dp[i + t], dp[i] + dis[c1][c2]);
-                }
+        dp[0] = 0;
+        for(int i = 0; i < n; i++) {
+            if(dp[i] == Long.MAX_VALUE) continue;
+            TrieNode node1 = root, node2 = root;
+            if(arr1[i] == arr2[i] && dp[i] < dp[i + 1]) dp[i + 1] = dp[i];
+            for(int j = i; j < n; j++) {
+                node1 = node1.next[arr1[j] - 'a'];
+                node2 = node2.next[arr2[j] - 'a'];
+                if(node1 == null || node2 == null) break;
+                if(node1.index != -1 && node2.index != -1 && dist[node1.index][node2.index] != Integer.MAX_VALUE && dist[node1.index][node2.index] + dp[i] < dp[j + 1]) dp[j + 1] = dist[node1.index][node2.index] + dp[i];
             }
         }
-        return dp[dp.length - 1] == Long.MAX_VALUE ? -1L : dp[dp.length - 1];
+        return dp[n] == Long.MAX_VALUE ? -1 : dp[n];
     }
+    private void insert(String s, TrieNode root) {
+        for(int i = 0; i < s.length(); i++) {
+            int current = s.charAt(i) - 'a';
+            if(root.next[current] == null) root.next[current] = new TrieNode();
+            root = root.next[current];
+        }
+        if(root.index == -1) root.index = index++;
+    }
+    private int getIndex(String s, TrieNode root) {
+        for(int i = 0; i < s.length(); i++) {
+            int current = s.charAt(i) - 'a';
+            root = root.next[current];
+        }
+        return root.index;
+    }
+}
+class TrieNode {
+    TrieNode[] next = new TrieNode[26];
+    int index = -1;
 }

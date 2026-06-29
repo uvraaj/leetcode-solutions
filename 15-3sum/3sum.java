31a6bd2e745183635c16b9fbc496bf62a2ded9ce
Yuvraj Guleria <yuvrajguleria45@gmail.com>
2026-06-29 15:09:38 -0500

    Time: 1640 ms (5.00%) | Memory: 56.1 MB (95.86%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,29 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int target = 0; //as stated in the question
-        Arrays.sort(nums);  //this will help finding the triplets easier
-        Set<List<Integer>> set = new HashSet<>();   //using set data structure, to remove duplicates
-        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> triple_set = new HashSet<>(); //set to store unique triplets
+        int n = nums.length;
 
-        for (int i = 0; i < nums.length; i++) {
-            int j = i + 1;
-            int k = nums.length - 1;
+        for (int i = 0; i < n; i++) {
+            Set<Integer> hash_set = new HashSet<>(); //set to store elements seen so far
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            for (int j = i+1; j < n; j++) {
+                //calculating the third element, needed to form the triplet
+                int third = -(nums[i] + nums[j]);
 
-                if (sum == target) {
-                    set.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                } else if (sum < target) {  //sum is smaller, we need greater numbers
-                    j++;
-                } else {    //sum is larger, we need smaller numbers
-                    k--;
+                if (hash_set.contains(third)) {
+                    //found the triplet
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(third);
+
+                    //sort the triplets to ensure uniqueness in set
+                    Collections.sort(temp);
+                    triple_set.add(temp);
                 }
+                //if it does not contains, then add it for future checks
+                hash_set.add(nums[j]);
             }
         }
-        result.addAll(set);
-        return result;
+
+        //converting the set into the List
+        List<List<Integer>> ans = new ArrayList<>(triple_set);
+        return ans;
     }
 }

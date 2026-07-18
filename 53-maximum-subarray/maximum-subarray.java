ca8575699caddba1e1977b363def53402a8d2318
Yuvraj Guleria <yuvrajguleria45@gmail.com>
2026-07-17 22:14:33 -0500

    Time: 1 ms (99.96%) | Memory: 77.3 MB (48.67%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -5,9 +5,12 @@ class Solution {
         long max_sum = Long.MIN_VALUE;
 
         for (int i = 0; i < n; i++) {
+            //add current element to the sum
             curr_sum += nums[i];
 
+            //update the sum, if curr_sum is greater than the max_sum
             if (curr_sum > max_sum) max_sum = curr_sum;
+            //reset the sum to zero, if it becomes less than zero
             if (curr_sum < 0) curr_sum = 0;
         }
         return (int) max_sum;
